7c2b8e1fd4913bb3f5c49b747d3f61eafc558cff
imanudevv <anuanudev100@gmail.com>
2025-08-08 14:27:00 +0530

    14-26

--- src/components/Contact.jsx
+++ src/components/Contact.jsx
@@ -1,9 +1,22 @@
-import React from 'react'
+import React from 'react';
+import './Contact.css';
 
 const Contact = () => {
   return (
-    <div>Contact</div>
-  )
-}
+    <>
+      <div className='container'>
+        <h1>Let's</h1>
+        <h2>Create Something</h2>
+        <h3>That Leaves a Mark</h3>
+      </div>
+      <div className='form'>
+        <form action="contact">
+          <label htmlFor="email">Your Email:</label>
+          <input type="email" id="email" name="email" />
+        </form>
+      </div>
+    </>
+  );
+};
 
-export default Contact
+export default Contact;
